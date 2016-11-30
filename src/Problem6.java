e5b671349c30285edb2f15c45ab2b11c3e1194c9
qnb14161 <qnb14161@uni.starth.ac.uk>
2016-11-30 19:55:24 +0000

    Problem 7 done

--- src/Problem6.java
+++ src/Problem6.java
@@ -2,8 +2,23 @@
 public class Problem6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int difference = squareOfSum(100)-sumOfSquares(100);
+		System.out.println(difference);
 	}
-
+	
+	private static int sumOfSquares(int number){
+		int sum = 0;
+		for(int i = 0; i <= number ; i++){
+			sum = sum + (i*i);
+		}
+		return sum;
+	}
+	
+	private static int squareOfSum(int number){
+		int sum = 0;
+		for(int i = 0; i <= number; i++){
+			sum += i;
+		}
+		return sum*sum;
+	}
 }
