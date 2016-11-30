81e54d75a49744a7005d69fdc3ddb1f28fd913f0
qnb14161 <qnb14161@uni.starth.ac.uk>
2016-11-30 10:42:05 +0000

    Fixed checkPalendrome function

--- src/LargestThreeDigitsPalendrome.java
+++ src/LargestThreeDigitsPalendrome.java
@@ -3,25 +3,29 @@ import java.util.ArrayList;
 public class LargestThreeDigitsPalendrome {
 
 	public static void main(String []args){
-		System.out.println(checkPelandrome("123456654321"));
+		System.out.println(checkPelandrome("123321"));
 	}
 
 
 	private static boolean checkPelandrome(String num){
 
-		boolean result = false;
-		char[] numberArr = num.toCharArray();
-		for(int i = 0; i < numberArr.length-1; i++){
-			for(int j = numberArr.length-1; j > i; j--){
-				if(numberArr[i] == numberArr[j]){
-					System.out.println("Comparing " + numberArr[i] + "With " + numberArr[j]);
-					result = true;
-					continue;
-				}
+		boolean result = false; //the result
+		char[] numberArr = num.toCharArray(); //array of digits
+		int lastChar = numberArr.length-1; // last digit in array
+		for(int i = 0; i < numberArr.length-1; i++){ // compare each digit 
+			if(numberArr[i] == numberArr[lastChar]){
+				System.out.println("Comparing " + numberArr[i] + "With " + numberArr[lastChar]);
+				lastChar--;
+				result = true;
+
+			}
+			else{
+				System.out.println(" FAILED Comparing " + numberArr[i] + "With " + numberArr[lastChar]);
 				result = false;
-			
+				break;
 			}
 		}
+
 		return result;
 	}
 }
